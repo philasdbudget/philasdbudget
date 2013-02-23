07bb88c02e1138d9051884b58b92822129d3622e
Chris Brown <chris@cmbrown.org>
2013-02-23 18:04:04 -0500

    Added geocoding for charter schools.

--- geocode_schools.py
+++ geocode_schools.py
@@ -3,13 +3,21 @@ from geopy import geocoders
 import csv
 
 school_csv = open('./SchoolMetaDataNotes/schoolinformation.csv', 'r')
-school_long_lat = open('./SchoolMetaDataNotes/schools_geocoded.csv', 'w')
+school_long_lat = open('./SchoolMetaDataNotes/schools_geocoded.csv', 'w+')
+charter_csv = open('/path/to/csv/', 'r')
 
 school_reader = csv.DictReader(school_csv)
+charter_reader = csv.DictReader(charter_csv)
 
 g = geocoders.Yahoo('YQEyGPDV34FG4bXzx_UKz0hQJjlnwOQ2pBjOPzIbnZ.xaMD9lln5zYAXkeyTj3O99K84AZmD6KX50BDkM57rx42T_chc0kg-')
 school_long_lat.write("school_code,lat,lng")
+
 for row in school_reader:
 	address = "%s %s" % (row['ADDRESS'], row['SCHOOL_ZIP'])
+	place, (lat, lng) = g.geocode(address)
+	school_long_lat.write("%s,%s,%s\n" %(row['SCHOOL_CODE'], lat, lng))
+
+for row in charter_reader:
+	address = "%s %s" % (address, school_zip)
 	place, (lat, lng) = g.geocode(address)
 	school_long_lat.write("%s,%s,%s\n" %(row['SCHOOL_CODE'], lat, lng))
