ae83a2b9b695cc271ed4cb229956458eab46be87
Adam Hinz <hinz.adam@gmail.com>
2013-02-24 01:09:59 -0500

    Basic flask app going

--- wsgi/models.py
+++ wsgi/models.py
@@ -1,104 +1,34 @@
-from sqlalchemy.ext.declarative import declarative_base
+from settings import USER, PASSWORD, DB, HOST
 from sqlalchemy import *
+# from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+engine = create_engine(
+  "postgresql://%s:%s@%s/%s" % (USER,PASSWORD,HOST,DB))
 
-class SchoolEnrollment(Base):
-    __tablename__ = 'SCHOOL_ENROLLMENT'
+metadata = MetaData(bind=engine)
 
-    school_code = Column(Integer, primary_key=True)
-    school_year = Column(String, primary_key=True)
-    sch_attendance = Column(Float)
-    sch_enrollment = Column(Integer)
-    sch_student_entered = Column(Integer)
-    sch_student_withdrew = Column(Integer)
+SchoolEnrollment = Table('school_enrollment', metadata, autoload=True)
 
-class SchoolEthnicityLowIncome(Base):
-    __tablename__ = "SCHOOL_ETHNICITY_LOW_INCOME"
+SchoolEthnicityLowIncome = Table('school_ethnicity_low_income',
+                                 metadata, autoload=True)
 
-    school_code = Column(Integer, primary_key=True)
-    school_year = Column(String, primary_key=True)
-    african_american = Column(Float)
-    white = Column(Float)
-    asian = Column(Float)
-    latino = Column(Float)
-    other = Column(Float)
-    pacific_islander = Column(Float)
-    american_indian = Column(Float)
-    sch_low_income_family = Column(Float)
+SchoolInformation = Table('school_information', metadata, autoload=True)
+SchoolPSSA = Table('school_pssa', metadata, autoload=True)
+SeriousIncidents = Table('school_serious_incidents', metadata, autoload=True)
+SchoolStudent = Table('school_student', metadata, autoload=True)
+SchoolSuspensions = Table('school_suspensions', metadata, autoload=True)
+TeacherAttend = Table('teacher_attend', metadata, autoload=True)
+Budget = Table('budget', metadata,
+               Column('ulcs', None, ForeignKey('ulcs.id')),
+               Column('item', None, ForeignKey('item.id')),
+               Column('snapshot', None, ForeignKey('snapshots.id')),
+               autoload=True)
+Item = Table('item', metadata, autoload=True)
+Snapshot = Table('snapshots', metadata, autoload=True)
+Ulcs = Table('ulcs', metadata, autoload=True)
 
-class SchoolInformation(Base):
-    __tablename__ = "SCHOOL_INFORMATION"
-
-    school_code = Column(Integer, primary_key=True)
-    school_name_1 = Column(String)
-    school_name_2 = Column(String)
-    address = Column(String)
-    school_zip = Column(Integer)
-    zip_plus_4 = Column(String)
-    city = Column(String)
-    state_cd = Column(String)
-    phone_number = Column(Integer)
-    sch_start_grade = Column(Integer)
-    sch_term_grade = Column(Integer)
-    hpaddr = Column(String)
-    school_level_name = Column(String)
-
-class SchoolPSSA(Base):
-    __tablename__ = "SCHOOL_PSSA"
-
-    school_code = Column(Integer, primary_key=True)
-    school_year = Column(String, primary_key=True)
-    grade = Column(Integer)
-    math_advanced_percent = Column(String)
-    math_proficient_percent = Column(String)
-    math_basic_percent = Column(String)
-    math_below_basic_percent = Column(String)
-    read_advanced_percent = Column(String)
-    read_proficient_percent = Column(String)
-    read_basic_percent = Column(String)
-    read_below_basic_percent = Column(String)
-    math_combined_percent = Column(String)
-    read_combined_percent = Column(String)
-
-class SeriousIncidents(Base):
-    __tablename__ = "SCHOOL_SERIOUS_INCIDENTS"
-
-    ulcs_no = Column(Integer, primary_key=True)
-    school_year = Column(String, primary_key=True)
-    assault = Column(Integer)
-    drug = Column(Integer)
-    morals = Column(Integer)
-    weapons = Column(Integer)
-    theft = Column(Integer)
-
-class SchoolStudent(Base):
-
-    __tablename__ = "SCHOOL_STUDENT"
-
-    school_code = Column(Integer, primary_key=True)
-    school_year = Column(String, primary_key=True)
-    sch_spec_ed_services = Column(Float)
-    sch_mg = Column(Float)
-    sch_esol_services = Column(Float)
-
-class SchoolSuspensions(Base):
-
-    __tablename__ = "SCHOOL_SUSPENSIONS"
-
-    school_code = Column(Integer, primary_key=True)
-    school_year = Column(String, primary_key=True)
-    total_suspensions = Column(Integer)
-    sch_one_time_susp = Column(Integer)
-    sch_two_time_susp = Column(Integer)
-    sch_three_time_susp = Column(Integer)
-    sch_more_than_three_susp = Column(Integer)
-
-class TeacherAttend(Base):
-
-    __tablename__ = "TEACHER_ATTEND"
-
-    school_code = Column(Integer, primary_key=True)
-    school_year = Column(String, primary_key=True)
-    sch_teacher_attend = Column(Float)
-    sdp_teacher_attend_avg = Column(Float)
+# conn = engine.connect()
+# for row in conn.execute(select([Budget.join(Item).join(Snapshot).join(Ulcs)]).limit(2)):
+#     print row
+# for row in conn.execute(select([SchoolPSSA], SchoolPSSA.c.school_code == '1580')):
+#     print row
