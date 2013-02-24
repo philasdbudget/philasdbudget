7cb4be6ba2980770c1487c6f84f7cfa5f2dbade7
Chris Brown <chris@cmbrown.org>
2013-02-23 20:34:39 -0500

    Added charter schools to geocoding script

--- geocode_schools.py
+++ geocode_schools.py
@@ -4,20 +4,28 @@ import csv
 
 school_csv = open('./SchoolMetaDataNotes/schoolinformation.csv', 'r')
 school_long_lat = open('./SchoolMetaDataNotes/schools_geocoded.csv', 'w+')
-charter_csv = open('/path/to/csv/', 'r')
-
+charter_csv = open('./SchoolMetaDataNotes/charterinformation.csv', 'rU')
 school_reader = csv.DictReader(school_csv)
 charter_reader = csv.DictReader(charter_csv)
 
 g = geocoders.Yahoo('YQEyGPDV34FG4bXzx_UKz0hQJjlnwOQ2pBjOPzIbnZ.xaMD9lln5zYAXkeyTj3O99K84AZmD6KX50BDkM57rx42T_chc0kg-')
 school_long_lat.write("school_code,lat,lng")
 
-for row in school_reader:
+print "Geocoding Philadelphia School District Schools"
+for counter, row in enumerate(school_reader):
+	if counter % 25 == 0:
+		print counter
 	address = "%s %s" % (row['ADDRESS'], row['SCHOOL_ZIP'])
 	place, (lat, lng) = g.geocode(address)
 	school_long_lat.write("%s,%s,%s\n" %(row['SCHOOL_CODE'], lat, lng))
 
-for row in charter_reader:
-	address = "%s %s" % (address, school_zip)
-	place, (lat, lng) = g.geocode(address)
+print "Geocoding Philadelphia Charter Schools"
+for counter, row in enumerate(charter_reader):
+	if counter % 25 == 0:
+		print counter
+	address = "%s %s" % (row['ADDRESS'], row['SCHOOL_ZIP'])
+	try:
+		place, (lat, lng) = g.geocode(address)
+	except Exception, e:
+		print "%s\nCode:%s\nAddress%s" % (e, row['SCHOOL_CODE'], address)
 	school_long_lat.write("%s,%s,%s\n" %(row['SCHOOL_CODE'], lat, lng))
