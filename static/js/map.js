5d7e39c981e1bfc7bf4d960ef4f8987e4a039bf5
Chris Brown <chris@cmbrown.org>
2013-02-24 15:39:45 -0500

    Added names to summaries.

--- static/js/map.js
+++ static/js/map.js
@@ -10,38 +10,38 @@ var school_points;
 var school_points = $.getJSON("/api/schools",
 	function(json) { school_points = json;});
 
-function create_handler(ulcs) {
+function create_handler(ulcs, name, address) {
 	return function () {
-		school_summary(ulcs);
+		school_summary(ulcs, name, address);
 	};
 };
 
 $(window).load(function() {
 	for (var i = 0; i < school_points.length; i++) {
 	    var ulcs = school_points[i].ulcs;
+	    var name = school_points[i].school_name;
+	    var address = school_points[i].address;
 	    var circle = L.circle([school_points[i].geom[1], 
 	    	school_points[i].geom[0]], 50, {
-	    	fillColor: 'red',
-	    	fillOpacity: 0.5,
-	    	id: school_points[i].ulcs,
 	    })
-	    .on('click', create_handler(ulcs))
+	    .on('click', create_handler(ulcs, name, address))
 	    .addTo(map);
 	};
 });
 
 
 
-function school_summary (ulcs) {
+function school_summary (ulcs, name, address) {
+	var summary_count = $(".school_summary").length;
+	if (summary_count == 3) {
+		$('.school_summary').first().remove();
+	}
 	$.getJSON("/api/budget/" + ulcs + "/181", function(data) {
-		var html_string;
-		console.log(data.items)
+		var html_string = "";
 		for (var i = 0; i < data.items.length; i++) {
-			console.log(data.items[i])
-			html_string += "<li>" + data.items[i].item + ":" + data.items[i].amount + "</li>";
+			html_string += "<li>" + data.items[i].item + ": $" + data.items[i].amount + "</li>";
 		};
-		div = '<div class="school_summary"><h1>' + ulcs + '</h1>' + '<ul>' + html_string + '</ul></div>'
-		console.log(div)
+		div = '<div class="school_summary span3"><h3>' + name + '</h3>' + '<h4>' + address + '</h4>' + '<ul>' + html_string + '</ul></div>'
 		$(".school-summaries").append(div);
 	});
 };
