cc249052d0032bbd76458f1f6032a0da71ff04a4
Adam Hinz <hinz.adam@gmail.com>
2013-02-28 21:05:47 -0500

    Make "all.html" into a vertical bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,18 @@
-var map = L.map('map').setView([39.958175, -75.160217], 13);
+// var map = L.map('map').setView([39.958175, -75.160217], 13);
 
-L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Map data &copy; <a href="http://www.esri.com/">ESRI</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-}).addTo(map);
+// function create_handler(ulcs) {
+//     return function() {
+//         alert(ulcs);
+//     }
+// }
 
-var school_points;
+// L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+//     attribution: 'Map data &copy; <a href="http://www.esri.com/">ESRI</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+//     maxZoom: 18
+// }).addTo(map);
 
-var school_points = $.getJSON("/js/schools.json", function(json) { school_points = json;});
+// var school_points;
 
-L.geoJson(school_points).addTo(map);
+// var school_points = $.getJSON("/js/schools.json", function(json) { school_points = json;});
+
+// L.geoJson(school_points).addTo(map);
